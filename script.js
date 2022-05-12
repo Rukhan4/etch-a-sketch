68ccccc5cb10de1a091e863bd2626ffab9d22096
Surur Khan <surur.rkhan@gmail.com>
2022-05-12 12:30:38 -0400

    grid on pagegit add .!

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const grid = document.querySelector(".grid");
 
-function createGrid() {
+function defaultGrid() {
     for (let i = 0; i < 256; i++) {
         const div = document.createElement("div");
         div.classList.add('cell');
         div.addEventListener('mouseover', function (e) {
             e.target.style.backgroundColor = "black";
         })
-        grid.append(div);
+        grid.appendChild(div);
     }
 };
 
@@ -25,3 +25,23 @@ function removeAllChildNodes(parent) {
         parent.removeChild(parent.firstChild);
     }
 }
+
+const slider = document.querySelector(".slider");
+const sliderValue = document.querySelector(".value");
+slider.addEventListener('input', function () {
+    let val = document.getElementById("slider").value;
+    sliderValue.textContent = val;
+    removeAllChildNodes(grid);
+    grid.setAttribute("style", `grid-template-columns: repeat(${val}, 2fr);
+    grid-template-rows: repeat(${val}, 2fr);`);
+    for (let j = 0; j < (val * val); j++) {
+        const div = document.createElement("div");
+        div.classList.add("cell");
+        div.addEventListener("mouseover", function (e) {
+            e.target.style.backgroundColor = "black";
+        })
+        grid.appendChild(div);
+    }
+});
+
+defaultGrid();
